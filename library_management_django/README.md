5b826f4e540ef95d367ef242b267dd1532a9f0f9
Manish3434 <maneeskumar3434@gmail.com>
2026-05-31 16:23:28 +0530

    khgjhg

---
+++ library_management_django/README.md
@@ -0,0 +1,14 @@
+# Django Library Management System
+
+## Installation
+
+```bash
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py runserver
+```
+
+## Features
+- Add books
+- Manage library records
+- Admin panel
---
+++ library_management_django/library_project/settings.py
@@ -0,0 +1,42 @@
+SECRET_KEY = 'django-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'library_app',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+ROOT_URLCONF = 'library_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+    },
+]
+
+WSGI_APPLICATION = 'library_project.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Kolkata'
+STATIC_URL = 'static/'
